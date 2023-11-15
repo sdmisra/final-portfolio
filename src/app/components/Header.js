a57fcd5655867e643e87e45f0dcde7d2fa73aca4
Shane Misra <shane.misra@gmail.com>
2023-11-15 12:21:00 -0700

    added devicon for responsive graphics on tech stack

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Header = ({setSelection})=> {
   return (
     <header className='sticky top-0 w-full border-b-4 bg-slate-200'>
-      <div className='flex justify-evenly'>
+      <div className='flex justify-evenly pt-2'>
         <a className='h-8' href='#welcome'>
           Home
         </a>
