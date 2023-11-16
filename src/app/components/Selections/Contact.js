8a94b76471dd2630edb3fbfb1d50e2a377648fd1
Shane Misra <shane.misra@gmail.com>
2023-11-15 19:23:00 -0700

    added email button to contact

--- src/app/components/Selections/Contact.js
+++ src/app/components/Selections/Contact.js
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import { motion } from 'framer-motion'
+import icon from '/public/emailicon.png'
 
 const Contact = () => {
   return (
@@ -6,10 +8,15 @@ const Contact = () => {
     className='h-auto flex w-full justify-center bg-slate-700/25 my-12 py-6 rounded drop-shadow-md'
     >
     <div>
-      <h1 id='contact' className="m-6 bg-slate-100 rounded p-2">
+      <h1 id='contact' className="m-6 bg-slate-200 rounded p-2">
         Have a project that needs another pair of hands on the keyboard?
       </h1>
-      <span></span>
+      <a href="mailto:shane.misra@gmail.com" className="flex flex-col items-center">
+        <Image src={icon} alt='email button'/>
+        <span className="bg-slate-200 rounded p-2">
+        Send me an Email!
+        </span>
+      </a>
     </div>
     </motion.section>
   )
