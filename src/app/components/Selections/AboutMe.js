ff434e52267ccd8d844f6ce80ee49ae11ed7af45
Shane Misra <shane.misra@gmail.com>
2023-11-16 11:30:05 -0700

    animations solid on welcome and aboutme

--- src/app/components/Selections/AboutMe.js
+++ src/app/components/Selections/AboutMe.js
@@ -19,7 +19,7 @@ const AboutMe = () => {
       <motion.p
         className='m-2 bg-slate-200/75 rounded p-2'
         initial={{ opacity: 0, y: -35 }}
-        animate={{ opacity: 1, y: 0, }}
+        whileInView={{ opacity: 1, y: 0, }}
         transition={{ delay: 4, duration: 2 }}
        >
       Most recently, my career centered around speciality cheeses in a high volume retail setting. It was challenging work throughout the holidays and the COVID19 pandemic particularly, but I find those experiences have helped me to anticipate the needs of the end user of my software in very practical ways.
@@ -27,26 +27,33 @@ const AboutMe = () => {
       <motion.p
         className='m-2 bg-slate-200/75 rounded p-2'
         initial={{ opacity: 0, y: -25 }}
-        animate={{ opacity: 1, y: 0, }}
+        whileInView={{ opacity: 1, y: 0, }}
         transition={{ delay: 6, duration: 2 }}
        >
       I find I thrive in collaborative environments in which I can ask questions, reach out for perspective and context, and learn from every mistake to grow steadily.
       </motion.p>
       <motion.p className='m-2 bg-slate-200/75 rounded p-2'
         initial={{ opacity: 0, y: -15 }}
-        animate={{ opacity: 1, y: 0, }}
+        whileInView={{ opacity: 1, y: 0, }}
         transition={{ delay: 9, duration: 2 }}
       >
       Please do not hestitate to reach out if you are looking for a software developer for projects and opportunities large or small. 
       </motion.p>
-      <span className="w-full flex justify-center">
+      <motion.span className="w-full flex justify-center"
+        initial={{ opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{
+          delay: 10,
+          type: 'tween',
+          duration: 2 }}
+      >
         <Link href="https://www.linkedin.com/in/shanemisra/" target="_blank" rel="noreferrer noopener" className="text-6xl">
         <i className="devicon-linkedin-plain"></i>
         </Link>
         <Link href="https://github.com/sdmisra" target="_blank" rel="noreferrer noopener" className="text-6xl">
         <i className="devicon-github-original"></i>
         </Link>
-      </span>
+      </motion.span>
     </div>
     <motion.div 
     className='flex justify-center w-2/5 border-b-2 border-slate-200 border-double'
--- src/app/components/Selections/Welcome.js
+++ src/app/components/Selections/Welcome.js
@@ -32,19 +32,19 @@ const Welcome = () => {
       }}>
       <h1 className="text-4xl mx-6 mb-8">Hello!</h1>
       <motion.p className='m-6 bg-slate-200/75 rounded p-2'
-        initial={{ opacity: 0, y: -25 }}
+        initial={{ opacity: 0, y: 25 }}
         animate={{ opacity: 1, y: 0, }}
         transition={{ delay: 5, duration: 2 }}>
         Welcome to my humble portfolio - it is no exaggeration that having visitors to this page is a dream come true for me, so thank you for taking the time to check it out.
       </motion.p>
       <motion.p className="m-6 bg-slate-200/75 rounded p-2"
-        initial={{ opacity: 0, y: -25 }}
+        initial={{ opacity: 0, y: 25 }}
         animate={{ opacity: 1, y: 0, }}
         transition={{ delay: 7, duration: 3 }}>
         My work so far in this field has a foundation in Frontend technologies, with an emphasis on the React framework. I am passionate about creating applications and tools that enable their users to learn and discover more about the world around us.
       </motion.p>
       <motion.p className='m-6 bg-slate-200/75 rounded p-2'
-        initial={{ opacity: 0, y: -25 }}
+        initial={{ opacity: 0, y: 25 }}
         animate={{ opacity: 1, y: 0, }}
         transition={{ delay: 10, duration: 2 }}>
         Please keep scrolling to learn more about me and the work I have done so far as a Software Engineer!
